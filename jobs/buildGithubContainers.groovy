54eb03bef1ae2ee8eac70d54fd96e746531393d4
stephencshelton <sshelton@sevatec.com>
2020-07-13 14:29:54 -0400

    Quoting keys

--- jobs/buildGithubContainers.groovy
+++ jobs/buildGithubContainers.groovy
@@ -1,11 +1,11 @@
 containers = [
-  JNLP:
+  'JNLP':
     [
       [name:'DOCKERFILE', defaultValue: 'resources/containers/Dockerfile-jnlp-openjdk11', description: 'Path to the dockerfile to build inside the github repository'],
       [name:'CONTAINER_NAME', defaultValue: 'stephenshelton/jnlp-agent', description: 'Name of repository on docker hub to push container to'],
       [name:'GITHUB_URL', defaultValue: 'https://github.com/stephencshelton/jenkins-jobdsl.git', description: 'URL for the github repository to checkout']
     ]
-  NOIP:
+  'NOIP':
     [
       [name:'DOCKERFILE', defaultValue: 'Dockerfile', description: 'Path to the dockerfile to build inside the github repository'],
       [name:'CONTAINER_NAME', defaultValue: 'stephenshelton/no-ip', description: 'Name of repository on docker hub to push container to'],

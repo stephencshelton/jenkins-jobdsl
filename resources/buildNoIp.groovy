01646efab6be4e9e66dda7d850db3ac8e8f0ee85
stephencshelton <sshelton@sevatec.com>
2020-07-13 13:52:27 -0400

    Changing Jenkinsfile base to correct file

--- resources/buildNoIp.groovy
+++ resources/buildNoIp.groovy
@@ -1,20 +1,24 @@
-pipeline {
-  environment {
-    registry = "stephenshelton/no-ip"
-    registryCredential = 'docker-creds'
-  }
-  agent any
-  stages {
-    stage('Cloning Git') {
-      steps {
-        git 'https://github.com/coppit/docker-no-ip.git'
-      }
-    }
-    stage('Building image') {
-      steps{
-        script {
-          dockerfile = "Dockerfile"
-          docker.build(registry + ":$BUILD_NUMBER", "-f ${dockerfile}")
+podTemplate(containers: [
+  containerTemplate(name: 'k3c', image: 'rancher/k3c:v0.2.1', privileged: true, ttyEnabled: true, command: 'k3c daemon')
+]) {
+  node(POD_LABEL) {
+    stage('Build jnlp-agent') {
+      git 'https://github.com/coppit/docker-no-ip'
+      container('k3c') {
+        stage('Build in k3c container') {
+          sh "k3c build -f Dockerfile -t stephenshelton/no-ip:0.0.${BUILD_NUMBER} ."
+          // Wait on k3c to show the new image, seems to be a little slow in that department
+          imageOutput = sh(script: "k3c images", returnStdout: true)
+          while(!imageOutput.contains("stephenshelton/no-ip")) {
+            echo "Waiting on image to be available to push"
+            sh "sleep 30"
+            println imageOutput
+            imageOutput = sh(script: "k3c images", returnStdout: true)
+          }
+        }
+        // Will add this once I have the JCasC creating the secrets for jenkins
+        stage('Push image to dockerhub') {
+          echo "k3c push stephenshelton/jnlp-agent:0.0.${BUILD_NUMBER}"
         }
       }
     }

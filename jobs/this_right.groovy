b7f9f837444943fb6c2a747227e5e2afc58bf511
Nathaniel Guthrie <nguthrie540@gmail.com>
2020-12-30 13:10:27 -0500

    My attempt

---
+++ jobs/this_right.groovy
@@ -0,0 +1,13 @@
+pipelineJob('My stupid shit') {
+    parameters {
+    container.getValue() { param ->
+    stringParam('discord', 'Posting as bot', 'Discord message'
+  }
+  }
+    definition {
+    cps {
+    script(readFileFromWorkspace('resources/first_bot.groovy'))
+    sandbox()
+  }
+  }
+  }
